a4cae5aa735040916dc09925a403fda43c6f9f99
XarCraftCoding <batu.erinckan1@gmail.com>
2024-01-23 13:25:27 +0300

    Update 2.0 - Growing a Parse Tree

---
+++ docs/grammar.md
@@ -0,0 +1,11 @@
+# Grammar
+
+**Warning:** This doesn't work on GitHub. If you want to see it, you will have to open this file in Visual Studio Code.
+
+$$
+\begin{align}
+[\text{exit}] &\to exit([\text{expr}]);
+\\
+[\text{expr}] &\to \text{int\_lit}
+\end{align}
+$$
---
+++ src/tokenization.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <string>
+#include <utility>
+#include <vector>
+
+enum class TokenType { exit, int_lit, semi };
+
+struct Token {
+    TokenType type;
+    std::optional<std::string> value {};
+};
+
+class Tokenizer {
+public:
+    inline explicit Tokenizer(std::string src)
+        : m_src(std::move(src))
+    {
+    }
+    
+    inline std::vector<Token> tokenize() {
+        std::vector<Token> tokens;
+        std::string buf;
+        while (peek().has_value()) {
+            if (std::isalpha(peek().value())) {
+                buf.push_back(consume());
+                while (peek().has_value() && std::isalnum(peek().value())) {
+                    buf.push_back(consume());
+                }
+                if (buf == "exit") {
+                    tokens.push_back({ .type = TokenType::exit });
+                    buf.clear();
+                    continue;
+                } else {
+                    std::cerr << "You messed up!" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+            
+            else if (std::isdigit(peek().value())) {
+                buf.push_back(consume());
+                while (peek().has_value() && std::isdigit(peek().value())) {
+                    buf.push_back(consume());
+                }
+                tokens.push_back({ .type = TokenType::int_lit, .value = buf });
+                buf.clear();
+                continue;
+            }
+            else if (peek().value() == ';') {
+                consume();
+                tokens.push_back({ .type = TokenType::semi });
+                continue;
+            }
+            else if (std::isspace(peek().value())) {
+                consume();
+                continue;
+            }
+            else {
+                std::cerr << "You messed up!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        m_index = 0;
+        return tokens;
+    }
+    
+private:
+    [[nodiscard]] inline std::optional<char> peek(int ahead = 1) const
+    {
+        if (m_index + ahead > m_src.length()) {
+            return {};
+        }
+        else {
+            return m_src.at(m_index);
+        }
+    }
+    
+    inline char consume()
+    {
+        return m_src.at(m_index++);
+    }
+    
+    const std::string m_src;
+    size_t m_index = 0;
+};
