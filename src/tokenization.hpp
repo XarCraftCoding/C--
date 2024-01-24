899cc896794a24a7080c9b9eae8d32f40cae988e
XarCraftCoding <batu.erinckan1@gmail.com>
2024-01-24 17:39:54 +0300

    Update 3.0 - Variables and Adding

--- src/tokenization.hpp
+++ src/tokenization.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <string>
-#include <utility>
 #include <vector>
 
-enum class TokenType { exit, int_lit, semi };
+enum class TokenType { exit, int_lit, semi, open_paren, close_paren, ident, let, eq, plus };
 
 struct Token {
     TokenType type;
@@ -17,8 +16,9 @@ public:
         : m_src(std::move(src))
     {
     }
-    
-    inline std::vector<Token> tokenize() {
+
+    inline std::vector<Token> tokenize()
+    {
         std::vector<Token> tokens;
         std::string buf;
         while (peek().has_value()) {
@@ -31,12 +31,18 @@ public:
                     tokens.push_back({ .type = TokenType::exit });
                     buf.clear();
                     continue;
-                } else {
-                    std::cerr << "You messed up!" << std::endl;
-                    exit(EXIT_FAILURE);
+                }
+                else if (buf == "let") {
+                    tokens.push_back({ .type = TokenType::let });
+                    buf.clear();
+                    continue;
+                }
+                else {
+                    tokens.push_back({ .type = TokenType::ident, .value = buf });
+                    buf.clear();
+                    continue;
                 }
             }
-            
             else if (std::isdigit(peek().value())) {
                 buf.push_back(consume());
                 while (peek().has_value() && std::isdigit(peek().value())) {
@@ -46,9 +52,29 @@ public:
                 buf.clear();
                 continue;
             }
+            else if (peek().value() == '(') {
+                consume();
+                tokens.push_back({ .type = TokenType::open_paren });
+                continue;
+            }
+            else if (peek().value() == ')') {
+                consume();
+                tokens.push_back({ .type = TokenType::close_paren });
+                continue;
+            }
             else if (peek().value() == ';') {
                 consume();
                 tokens.push_back({ .type = TokenType::semi });
+                continue;
+            }
+            else if (peek().value() == '=') {
+                consume();
+                tokens.push_back({ .type = TokenType::eq });
+                continue;
+            }
+            else if (peek().value() == '+') {
+                consume();
+                tokens.push_back({ .type = TokenType::plus });
                 continue;
             }
             else if (std::isspace(peek().value())) {
@@ -63,23 +89,23 @@ public:
         m_index = 0;
         return tokens;
     }
-    
+
 private:
-    [[nodiscard]] inline std::optional<char> peek(int ahead = 1) const
+    [[nodiscard]] inline std::optional<char> peek(int offset = 0) const
     {
-        if (m_index + ahead > m_src.length()) {
+        if (m_index + offset >= m_src.length()) {
             return {};
         }
         else {
-            return m_src.at(m_index);
+            return m_src.at(m_index + offset);
         }
     }
-    
+
     inline char consume()
     {
         return m_src.at(m_index++);
     }
-    
+
     const std::string m_src;
     size_t m_index = 0;
 };
